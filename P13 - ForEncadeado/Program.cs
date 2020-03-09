63aae648f9c274ca5478c5d063c3a6f0005bc99d
Black <viniibl4ck@gmail.com>
2020-03-09 17:20:16 -0300

    Mexendo um pouco mais com FOR

---
+++ P13 - ForEncadeado/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace P13___ForEncadeado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Projeto 13");
+
+            //Fazendo uma meia arvore 
+            for (int contadorLinha = 0; contadorLinha < 10; contadorLinha++)
+            {
+                for (int contadorColuna = 0; contadorColuna < 10; contadorColuna++)
+                {
+                    Console.Write("*");
+                    if (contadorColuna >= contadorLinha)
+                    {
+                        break;
+                    }
+                }
+                Console.WriteLine();
+            }
+
+            //Fazendo a arvore de uma forma diferente
+            for (int contadorLinha = 0; contadorLinha < 10; contadorLinha++)
+            {
+                for (int contadorColuna = 0; contadorColuna < contadorLinha; contadorColuna++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
