5f95da3626305ac78f62de7b6c235ef189784790
Black <viniibl4ck@gmail.com>
2020-03-09 16:31:02 -0300

    Aprendendo a suar o FOR

---
+++ P11 - CalculaPopanca2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace P11___CalculaPopanca2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 11");
+
+            double valorIvestido = 1000;
+
+
+            for (int contadorMes = 1; contadorMes <= 12; contadorMes++)
+            {
+
+                valorIvestido = valorIvestido + valorIvestido * 0.0036;
+                Console.WriteLine("Após " + contadorMes + " meses, você terá R$" + valorIvestido);
+
+            }
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
