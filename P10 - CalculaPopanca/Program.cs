92fce75107ed9f74d5d3b33fd4d3ad42a596bb11
Black <viniibl4ck@gmail.com>
2020-03-09 16:30:11 -0300

    aprendndo a usar o while

---
+++ P10 - CalculaPopanca/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace P10___CalculaPopanca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Projeto 10 - Calcula poupança");
+
+            double valorIvestido = 1000;
+            int contadorMes = 1;
+
+            while (contadorMes <= 12)
+            {
+                valorIvestido = valorIvestido + valorIvestido * 0.0036;
+                Console.WriteLine("Após " + contadorMes + " meses, você terá R$" + valorIvestido);
+                contadorMes++;
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
