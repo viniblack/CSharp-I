1cd09d43add6404e4ff2a0086d1b061b2112f64f
Black <viniibl4ck@gmail.com>
2020-03-09 11:28:50 -0300

    Sobindo exercios da aula

---
+++ 6-AtribuicoesDeVariaveis/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _6_AtribuicoesDeVariaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto ");
+
+            int idade = 32;
+            int idadeGustavo = idade;
+
+            Console.WriteLine(idade);
+            Console.WriteLine(idadeGustavo);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 7-Condicionais/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _7_Condicionais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 7 - Condicionais");
+
+            int idadeJoao = 16;
+            int quatidadePessoas = 1;
+
+            if (idadeJoao >= 18)
+            {
+                Console.WriteLine("João possui mais de 18 anos de idade, pode entrar.");
+            }
+            else
+            {
+                if (quatidadePessoas >= 2)
+                {
+                    Console.WriteLine("João possui menos de 18 anos, mas esta acompanhado");
+                }
+                else
+                {
+                    Console.WriteLine("João possui menos de 18 anos de idade e não esta acompanhado , não pode entrar.");
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
