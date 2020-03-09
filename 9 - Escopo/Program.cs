6558e76924ebf4f7a30cf095cdabfcc65ac4f72a
Black <viniibl4ck@gmail.com>
2020-03-09 13:59:45 -0300

    Aprendendo IF, ELSE

---
+++ 9 - Escopo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _9_Escopo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 9 - Escopo");
+
+            int idadeJoao = 18;
+            bool acompanhado = true;
+            string mensagemAdicional;
+
+            if (acompanhado == true)
+            {
+                 mensagemAdicional = "João está acompanhado!";
+            }
+            else
+            {
+                mensagemAdicional = "João NÃO está acompanhado!";
+
+            }
+            if (idadeJoao >= 18 || acompanhado == true)
+            {
+                Console.WriteLine("Pode entrar.");
+                Console.WriteLine(mensagemAdicional);
+            }
+            else
+            {
+                Console.WriteLine("Não pode entrar.");
+            }
+
+            Console.ReadLine(); 
+        }
+    }
+}
