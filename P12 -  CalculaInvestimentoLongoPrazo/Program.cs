21e6bcbc2c06780599af9788a3ae438232ff0220
Black <viniibl4ck@gmail.com>
2020-03-09 17:19:40 -0300

    Encadeando laços FOR

---
+++ P12 -  CalculaInvestimentoLongoPrazo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace P12____CalculaInvestimentoLongoPrazo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 12");
+
+            double valorInvestido = 1000;
+            double fatorRedimento = 1.0036;
+
+            for(int contadorAno = 1; contadorAno <= 5; contadorAno++)
+            {
+                for (int contadorMes = 1; contadorMes < 12; contadorMes++)
+                {
+                    valorInvestido *= fatorRedimento;
+                }
+                fatorRedimento += 0.0010;
+            }
+
+            Console.Write("Ao término do investimento, você terá R$" + valorInvestido);
+
+            Console.ReadLine();
+        }
+    }
+}
